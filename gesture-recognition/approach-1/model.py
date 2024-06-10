5719f2af91d955697d4fd6fde73a1185ae836bd5
anajovanoviic <jovanovic.ana9898@gmail.com>
2024-06-10 18:18:17 +0200

    fixes #2

--- gesture-recognition/approach-1/model.py
+++ gesture-recognition/approach-1/model.py
@@ -1,39 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Convolution2D, Activation, Dropout
 
-#height = 480
-#width = 640
-#depth = 1
-
-#
 
 def create_model(height, width, depth, num_classes):
     
     #this architecture recalls on AlexNet a bit and on https://www.tensorflow.org/tutorials/images/cnn
     model = Sequential() #Sequential class groups a linear stack 
     #try without padding
-    model.add(Conv2D(32, (7, 7), input_shape=(height, width, depth), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    #model.add(Conv2D(32, (7, 7), input_shape=(height, width, depth), padding='same', activation='relu'))
+    model.add(Conv2D(32, (7, 7), input_shape=(depth, height, width), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), padding='same'))
+    model.add(MaxPooling2D(pool_size=(2,2), padding='same'))
     model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(MaxPooling2D(pool_size=(2,2), padding='same'))
     model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
     model.add(Flatten())
     model.add(Dense(64, activation='relu'))
-    model.add(Dense(num_classes))
+    model.add(Dense(num_classes, activation='softmax'))
     
     return model
 
-'''
-model = create_model(height, width, depth, num_classes)
-model.summary()
 
-#change loss when you add more gestures, i.e. more multi-class classification
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-history = model.fit()
-'''
-    
     
   
